d320da8348598d4adb6d500265d4d112189b91d8
Serena Leung <serenaleung1@gmail.com>
2019-10-15 22:41:37 -0700

    Updating result based on condition and passing this to handleIncrement()

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: []
+  };
+
+  handleIncrement = () => {
+    console.log('Increment Clicked', this);
   };
 
   render() {
-    let classes = this.getBadgeClasses();
-
     return (
       <div>
-        <span className={classes}>{this.formatCount()}</span>
-        <button className='btn btn-secondary btn-sm'>Increment</button>
+        {this.state.tags.length === 0 && 'Please create a new tag!'}
+        {this.renderTags()}
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        <button
+          onClick={this.handleIncrement}
+          className='btn btn-secondary btn-sm'
+        >
+          Increment
+        </button>
       </div>
     );
   }
@@ -24,6 +34,17 @@ class Counter extends Component {
   formatCount() {
     const { count } = this.state;
     return count === 0 ? 'Zero' : count;
+  }
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
   }
 }
 
