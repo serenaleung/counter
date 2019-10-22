68e5a2bd0ba522bcfafd6c46e3d5559e650b01b6
Serena Leung <serenaleung1@gmail.com>
2019-10-21 18:06:44 -0700

    Refactor

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 class Counter extends Component {
   state = {
-    value: this.props.value
+    value: this.props.counter.value
     // tags: []
   };
 
@@ -26,7 +26,7 @@ class Counter extends Component {
           Increment
         </button>
         <button
-          onClick={() => this.props.onDelete(this.props.id)}
+          onClick={() => this.props.onDelete(this.props.counter.id)}
           className='btn btn-danger btn-sm m-2'
         >
           Delete
