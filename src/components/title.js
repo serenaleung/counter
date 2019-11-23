d6cd1fccdbf788ed18266e1d226f0bd275f82169
Serena Leung <serenaleung1@gmail.com>
2019-11-23 15:49:55 -0800

    Update using input/hook

--- src/components/title.js
+++ src/components/title.js
@@ -2,10 +2,16 @@ import React, { useState } from 'react';
 
 export default function Title() {
   const [title, updateTitle] = useState('my Title');
+  const [inputVal, updateInput] = useState('');
   return (
     <div>
-      {title}
-      <button onClick={() => updateTitle('NEW')}>Button</button>
+      <h3>{title}</h3>
+      <input
+        type='text'
+        value={inputVal}
+        onChange={e => updateInput(e.target.value)}
+      />
+      <button onClick={() => updateTitle(inputVal)}>Button</button>
     </div>
   );
 }
