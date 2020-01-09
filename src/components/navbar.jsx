9f40d4df0b2d7b1a3cc9bc06bfa984dff23070aa
Serena Leung <serenaleung1@gmail.com>
2020-01-08 19:30:02 -0800

    Add search in list and update navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,6 +10,21 @@ const NavBar = ({ totalCounters }) => {
           {totalCounters}
         </span>
       </a>
+      <form className='form-inline'>
+        <input
+          className='form-control mr-sm-2'
+          type='search'
+          placeholder='Search'
+          aria-label='Search'
+        />
+        <button
+          className='btn btn-outline-secondary my-2 my-sm-0'
+          type='submit'
+        >
+          Search
+        </button>
+        <a href='#'>Cart (0)</a>
+      </form>
     </nav>
   );
 };
