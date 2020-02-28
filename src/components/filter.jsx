5dd31c92b443d543a7b78114f70f44fb9fdcddbc
Serena Leung <serenaleung1@gmail.com>
2020-02-27 23:29:13 -0800

    Filter logic pt1

--- src/components/filter.jsx
+++ src/components/filter.jsx
@@ -1,19 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Collapse } from '@blueprintjs/core';
 
-export default function Filter() {
+export default function Filter(props) {
+  const [isOpen, setIsOpen] = useState(true);
+  const [clicked, setClicked] = useState(false);
+  const sizes = ['XS', 'S', 'M', 'L', 'Xl', 'XXL'];
+
+  const handleOpenClick = () => {
+    setIsOpen(!isOpen);
+  };
+
+  const handleClassClick = () => {
+    setClicked(!clicked);
+    getSizeClasses();
+  };
+
+  const getSizeClasses = i => {
+    let sizeClasses = 'filterSizeButton ';
+    sizeClasses += clicked ? 'filterSizeButtonClicked' : '';
+    return sizeClasses;
+  };
+
   return (
     <div className='aside'>
-      <h3>Size</h3>
       <div className='row'>
-        <span className='filterSizeButton'>XS</span>
-        <span className='filterSizeButton'>S</span>
-        <span className='filterSizeButton'>M</span>
-        <span className='filterSizeButton'>L</span>
+        <button className='bp3-button bp3-minimal' onClick={handleOpenClick}>
+          <h3>Size</h3>
+          <span className='bp3-icon bp3-icon-plus justifyEnd'></span>
+        </button>
       </div>
-      <div className='row'>
-        <span className='filterSizeButton'>XL</span>
-        <span className='filterSizeButton'>XXL</span>
-      </div>
+      <Collapse isOpen={!isOpen}>
+        <div className='row'>
+          {sizes.map((size, i) => (
+            <div
+              className={getSizeClasses()}
+              key={i}
+              onClick={handleClassClick}
+            >
+              {size}
+            </div>
+          ))}
+        </div>
+      </Collapse>
     </div>
   );
 }
